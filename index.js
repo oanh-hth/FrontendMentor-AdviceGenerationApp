4a8ae22d0d79093cdb726135ebfd74bc6cbc3485
Oanh <hthoanh.95@gmail.com>
2022-07-19 21:52:34 +0700

    edit img+btn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const getData = async () => {
     <h1>"${slip.advice}"</h1>
     <picture>
         <source media="(min-width: 768px)" srcset="./images/pattern-divider-desktop.svg">
-        <img src="./images/pattern-divider-mobile.svg">
+        <img src="./images/pattern-divider-mobile.svg" alt="">
     </picture>
-    <button id='btn'>
+    <button id="btn" aria-label="change button">
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
     </button>
 
